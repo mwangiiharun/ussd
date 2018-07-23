b05b888433afb261b8a1c0b5f80b1faa060f34ce
mwangiiharun <harun.mwangi@msurvey.co>
2018-07-23 11:30:16 +0300

    Added logger to see request params

--- src/main/java/org/msurvey/ussd/web/UssdController.java
+++ src/main/java/org/msurvey/ussd/web/UssdController.java
@@ -1,5 +1,6 @@
 package org.msurvey.ussd.web;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("v1")
 public class UssdController {
+    private final Logger logger=Logger.getLogger(this.getClass());
 
 
     @RequestMapping(method = RequestMethod.GET, value = "/ussd", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<HashMap<String, Object>> processUssd (@RequestParam (required = false) String request){
+    public ResponseEntity<HashMap<String, Object>> processUssd (@RequestParam (required = false) Map<String, String> params){
         HashMap<String, Object> response= new HashMap<>();
+        logger.info(params);
         response.put("Status", true);
         response.put("Message", "Success");
         return new ResponseEntity<>(response, HttpStatus.OK);
